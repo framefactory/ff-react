402738459f59bfdcf36bacb1acfd50bf1f2cae8f
Ralph Wiedemeier <ralph@framefactory.ch>
2020-05-01 16:44:24 +0200

    updated configuration

--- test/test.ts
+++ test/test.ts
@@ -5,8 +5,23 @@
  * License: MIT
  */
 
+import * as path from "path";
+import * as moduleAlias from "module-alias";
+
+moduleAlias.addAliases({
+    "@ff/react": path.resolve(__dirname, "../export"),
+    "@ff/core": path.resolve(__dirname, "../../../ff-core/built/export"),
+    "@ff/browser": path.resolve(__dirname, "../../../ff-browser/built/export"),
+    "@ff/graph": path.resolve(__dirname, "../../../ff-graph/built/export"),
+});
+
+// define vars on node global object (usually done by Webpack)
+global["ENV_DEVELOPMENT"] = false;
+global["ENV_PRODUCTION"] = true;
+global["ENV_VERSION"] = "Test";
+
 ////////////////////////////////////////////////////////////////////////////////
 
 suite("FF React", function() {
-    // No tests
+    // no tests
 });
